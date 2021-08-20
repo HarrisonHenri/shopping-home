384758849ce543ebb14b80cb0248b01b63702955
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-08-20 16:00:58 -0300

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Suspense} from 'react';
+import Header from 'nav/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Suspense fallback="Loading...">
+    <body className="bg-gray-100">
+      <div className="container mx-auto mt-10">
+        <div className="flex shadow-md my-10">
+          <div className="w-3/4 bg-white px-10 py-10">
+            <Header />
+          </div>
+        </div>
+      </div>
+    </body>
+  </Suspense>
+);
 
-export default App;
+export default App
